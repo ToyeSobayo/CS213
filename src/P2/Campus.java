2f26300c69fe8eddd860166d96460963ed874369
Toye Sobayo <sobayotoye482@gmail.com>
2023-10-06 23:17:25 -0400

    Campus class done

--- src/P2/Campus.java
+++ src/P2/Campus.java
@@ -1,2 +1,21 @@
-package P2;public enum Campus {
+package P2;
+
+
+public enum Campus {
+
+    NEW_BRUNSWICK(0),
+    NEWARK(1),
+
+    CAMDEN(2);
+
+    private final int code;
+    // Constructor for campus codes
+    private Campus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
 }
