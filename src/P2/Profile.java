b07ed6581c5220bc9aacfb256997ea5730698175
Toye Sobayo <sobayotoye482@gmail.com>
2023-10-08 20:06:22 -0400

    Implemented the profile class

--- src/P2/Profile.java
+++ src/P2/Profile.java
@@ -1,2 +1,49 @@
-package P2;public class Profile {
+package P2;
+
+public class Profile implements Comparable<Profile> {
+
+    private  String fname;
+    private  String lname;
+    private  Date dob;
+
+    /**
+     * Constructor for a profile.
+     *
+     * @param fname First name of the account holder.
+     * @param lname Last name of the account holder.
+     * @param dob of birth of the account holder.
+     */
+    private Profile(String fname, String lname, Date dob) {
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+    }
+
+    public String getfname() {
+        return fname;
+    }
+
+    public String getlname() {
+        return lname;
+    }
+
+    /**
+     * Compares this profile to another profile.
+     *
+     * @param otherProfile The other profile to compare to.
+     * @return A negative integer, zero, or a positive integer if this profile is less than, equal to, or greater than the specified profile respectively.
+     */
+    @Override
+    public int compareTo(Profile otherProfile) {
+        int lastNameComparison = this.lname.compareTo(otherProfile.lname);
+        if (lastNameComparison != 0) {
+            return lastNameComparison;
+        }
+
+        int firstNameComparison = this.fname.compareTo(otherProfile.fname);
+        if (firstNameComparison != 0) {
+            return firstNameComparison;
+        }
+        return this.dob.compareTo(otherProfile.dob);
+    }
 }
