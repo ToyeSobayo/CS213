4a96794e6780c1496bb7217f2eedcf6b1080e44b
Toye Sobayo <sobayotoye482@gmail.com>
2023-10-08 20:04:59 -0400

    implemented the checking class

--- src/P2/Checking.java
+++ src/P2/Checking.java
@@ -1,2 +1,48 @@
-package P2;public class Checking {
+package P2;
+/**
+ * Represents a checking account.
+ *
+ * @author [Toye Sobayo]
+ */
+public class Checking extends Account {
+    // Constants for interest rate and fee for Checking accounts
+    private static final double INTEREST = 0.01;
+    private static final double MONTHLY_FEE = 12;
+    private  static final double FEE_WAIVER_BALANCE = 1000;
+
+
+
+    /**
+     * Constructor for a checking account.
+     *
+     * @param holder The profile of the account holder.
+     * @param balance The starting balance of the account.
+     */
+
+    public Checking(Profile holder, double balance) {
+        super(holder, balance);
+    }
+    /**
+     * Calculates the monthly interest for the checking account.
+     *
+     * @return The monthly interest.
+     */
+
+    @Override
+    public double monthlyInterest() {
+        return balance * INTEREST / 12;
+    }
+
+    @Override
+    public double monthlyFee() {
+        if (balance >= FEE_WAIVER_BALANCE) {
+            return 0;
+        }
+        return MONTHLY_FEE;
+    }
+
+    @Override
+    public int compareTo(Account o) {
+        return 0;
+    }
 }
