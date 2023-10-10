71acaf61a12d2b6afd1698d1bed19147e721cbd3
Toye Sobayo <sobayotoye482@gmail.com>
2023-10-09 23:19:22 -0400

    what I have so far for college checking, might need to do more idk yet

--- src/P2/CollegeChecking.java
+++ src/P2/CollegeChecking.java
@@ -1,2 +1,48 @@
-package P2;public class CollegeChecking {
+package P2;
+
+public class CollegeChecking extends Checking {
+
+    private static final double INTEREST_RATE_COLLEGE_CHECKING = 0.01;
+    private static final double FEE_COLLEGE_CHECKING = 0;
+
+    // Enum to represent different campuses?
+    public enum Campus {
+        NEW_BRUNSWICK, NEWARK, CAMDEN
+    }
+
+    private Campus campus;
+
+    /**
+     * Constructor for College checking account.
+     *
+     * @param holder  The profile of the account holder.
+     * @param balance The starting balance of the account.
+     */
+    public CollegeChecking(Profile holder, double balance, Campus campus) {
+        super(holder, balance);
+        this.campus = campus;
+    }
+
+    /**
+     * Calculates the monthly interest for the college checking account.
+     *
+     * @return The monthly interest.
+     */
+
+    @Override
+    public double monthlyInterest() {
+        return balance * INTEREST_RATE_COLLEGE_CHECKING / 12;
+    }
+    /**
+     * Calculates the monthly fee for the college checking account.
+     *
+     * @return The monthly fee.
+     */
+    @Override
+    public double monthlyFee() {
+        return FEE_COLLEGE_CHECKING;
+    }
+
+
+
 }
