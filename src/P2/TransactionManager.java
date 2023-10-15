5d70fb5c6f747c1d5c188669a5bb0a8e533276ee
Sean Thomas <spthomas127@gmail.com>
2023-10-15 15:00:50 -0400

    More modularization and implemented remove

--- src/P2/TransactionManager.java
+++ src/P2/TransactionManager.java
@@ -14,15 +14,35 @@ public class TransactionManager {
 
         while (scanner.hasNext()) {
             String command = scanner.next();
+            String input;
 
             switch (command) {
                 case "O":
-                    String input = scanner.nextLine();
-                    verifyThenOpen(input);
+                    input = scanner.nextLine();
+                    openAccount(input);
                     break;
 
                 case "C":
-                    scanner.nextLine();
+                    input = scanner.nextLine();
+                    Account account = verifyThenClose(input);
+
+                    if (account == null) {
+                        break;
+                    }
+
+                    String accountString = account.toString();
+                    if (db.contains(account)) {
+                        if (db.close(account)) {
+                            System.out.println(account.toString() + " has been closed.");
+                        }
+                        else {
+                            System.out.println(accountString + " is not in the database.");
+                        }
+                    }
+                    else {
+                        System.out.println(accountString + " is not in the database.");
+                    }
+
                     break;
 
                 case "D":
@@ -76,12 +96,14 @@ public class TransactionManager {
 
     }
 
-    private void verifyThenOpen(String input) {
+    private Account verifyThenOpen(String input) {
+        Account account = null;
+
         String[] accountParts = input.strip().split("\\s+");
 
         if (accountParts.length < 5) {
             System.out.println("Missing data for opening an account.");
-            return;
+            return null;
         }
         String type = accountParts[0];
 
@@ -100,51 +122,42 @@ public class TransactionManager {
             balance = Double.parseDouble(accountParts[4]);
             if (balance <= 0) {
                 System.out.println("Initial deposit cannot be 0 or negative.");
-                return;
+                return null;
             }
         }
 
         catch (NumberFormatException e){
             System.out.println("Not a valid amount.");
-            return;
+            return null;
         }
 
         Date dob = new Date(month, day, year);
 
         if (!dob.isValid()) {
             System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
-            return;
+            return null;
         }
 
         if (dob.isFuture()) {
             System.out.println("DOB invalid: " + dob.toString() + " cannot be today or a future day.");
-            return;
+            return null;
         }
 
         if (dob.underSixteen()) {
             System.out.println("DOB invalid: " + dob.toString() + " under 16.");
-            return;
+            return null;
         }
 
         Profile profile = new Profile(fname, lname, dob);
 
         if (type.equals("C")) {
-            Account account = new Checking(profile, balance);
-
-            if (db.open(account)) {
-                System.out.println(account.toString() + " opened.");
-            }
-
-            else {
-                System.out.println(account.toString() + " is already in the database.");
-            }
-
+            account = new Checking(profile, balance);
         }
 
         else if (type.equals("CC")) {
             if (accountParts.length < 6) {
                 System.out.println("Missing data for opening an account.");
-                return;
+                return null;
             }
 
             int cc = Integer.parseInt(accountParts[5]);
@@ -153,23 +166,15 @@ public class TransactionManager {
 
             if (campus == null) {
                 System.out.println("Invalid campus code.");
-                return;
+                return null;
             }
 
             if (dob.overTwentyFour()) {
                 System.out.println("DOB invalid: " + dob.toString() + " over 24.");
-                return;
+                return null;
             }
 
-            Account account = new CollegeChecking(profile, balance, campus);
-
-            if (db.open(account)) {
-                System.out.println(account.toString() + " opened.");
-            }
-
-            else {
-                System.out.println(account.toString() + " is already in the database.");
-            }
+            account = new CollegeChecking(profile, balance, campus);
 
 
         }
@@ -183,32 +188,113 @@ public class TransactionManager {
 
             boolean isLoyal = loyaltyCode == 1;
 
-            Account account = new Savings(profile, balance, isLoyal);
+            account = new Savings(profile, balance, isLoyal);
 
-            if (db.open(account)) {
-                System.out.println(account.toString() + " opened.");
-            }
-
-            else {
-                System.out.println(account.toString() + " is already in the database.");
-            }
         }
 
         else if (type.equals("MM")) {
             if (balance < 2000) {
                 System.out.println("Minimum of $2000 to open a Money Market account.");
-                return;
+                return null;
             }
 
-            Account account = new MoneyMarket(profile, balance, true);
+            account = new MoneyMarket(profile, balance, true);
 
-            if (db.open(account)) {
-                System.out.println(account.toString() + " opened.");
+        }
+
+        return account;
+    }
+
+    private Account verifyThenClose(String input) {
+        Account account = null;
+
+        String[] accountParts = input.strip().split("\\s+");
+
+        int TOKENS = 4;
+
+        if (accountParts.length < TOKENS) {
+            System.out.println("Missing data for closing an account.");
+            return null;
+        }
+        String type = accountParts[0];
+
+        String fname = accountParts[1];
+        String lname = accountParts[2];
+        String date = accountParts[3];
+
+        String[] dobParts = date.split("/");
+        int month = Integer.parseInt(dobParts[0]);
+        int day = Integer.parseInt(dobParts[1]);
+        int year = Integer.parseInt(dobParts[2]);
+
+        Date dob = new Date(month, day, year);
+
+        if (!dob.isValid()) {
+            System.out.println("DOB invalid: " + dob.toString() + " not a valid calendar date!");
+            return null;
+        }
+
+        if (dob.isFuture()) {
+            System.out.println("DOB invalid: " + dob.toString() + " cannot be today or a future day.");
+            return null;
+        }
+
+        if (dob.underSixteen()) {
+            System.out.println("DOB invalid: " + dob.toString() + " under 16.");
+            return null;
+        }
+
+        Profile profile = new Profile(fname, lname, dob);
+
+        if (type.equals("C")) {
+            account = new Checking(profile);
+        }
+
+        else if (type.equals("CC")) {
+            if (accountParts.length < TOKENS) {
+                System.out.println("Missing data for closing an account.");
+                return null;
             }
 
-            else {
-                System.out.println(account.toString() + " is already in the database.");
+            if (dob.overTwentyFour()) {
+                System.out.println("DOB invalid: " + dob.toString() + " over 24.");
+                return null;
             }
+
+            account = new CollegeChecking(profile);
+
+        }
+
+        else if (type.equals("S")) {
+            if (accountParts.length < TOKENS) {
+                System.out.println("3Missing data for closing an account.");
+                return null;
+            }
+
+            account = new Savings(profile);
+
+        }
+
+        else if (type.equals("MM")) {
+            account = new MoneyMarket(profile);
+        }
+
+        return account;
+    }
+
+    private void openAccount(String input) {
+        Account account = verifyThenOpen(input);
+
+        if (account == null) {
+            return;
+        }
+
+        if (db.open(account)) {
+            System.out.println(account.toString() + " opened.");
+        }
+
+        else {
+            System.out.println(account.toString() + " is already in the database.");
         }
     }
 
