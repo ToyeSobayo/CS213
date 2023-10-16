4df13f2c226a2b4990cd703b1c410bb6fe094b30
Sean Thomas <spthomas127@gmail.com>
2023-10-16 19:48:54 -0400

    Added documentation to run

--- src/P2/RunProject2.java
+++ src/P2/RunProject2.java
@@ -1,7 +1,10 @@
 package P2;
 
+/**
+ * Entry point to the program
+ * @author [Toye Sobayo, Sean Thomas]
+ */
 public class RunProject2 {
-
 
     public static void main(String[] args) {
         new TransactionManager().run();
